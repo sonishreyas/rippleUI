4eb1b4401ac1c7dbd527526caa88846ff78d393e
Shreyas Soni <sonishreyas10@gmail.com>
2022-02-01 02:32:11 +0530

    cards initial commit

---
+++ components/cards/src/js/properties.js
@@ -0,0 +1,17 @@
+/*
+ * rootCodeLocation: Where we need to import code for code tabs.
+ * elementCodeFilePath: The File Path for the component html file we need to import.
+ * elementCodeLocation: The location where we need to import the code for the component.
+ * handleCodeTabBtnClass: The classPath for the code tabs to call the handleCodeTab function which allows us to use the tabs.
+ * handleCodeTabCodeContentClass: Here we add content classpath
+ * elementCodeClassPath: This is the location where we store the HTML code.
+ * elementCodeInnerText: Here we add HTML code
+ * elementJSCodeClassPath: Here we add the classpath for JS code.
+ * elementJSCodeInnerText: Here we add the JS code.
+ * sandboxURLClass: Here we add the classPath for sandboxURL.
+ * sandboxURL: Here we add  the sandbox URL
+ */
+
+const elements = [];
+
+export { elements };
