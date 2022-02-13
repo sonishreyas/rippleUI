ddd584e17dc6957bdf4b827974a3575711429f8f
Shreyas Soni <sonishreyas10@gmail.com>
2022-02-13 23:59:06 +0530

    ref: drawer code updated

--- components/drawers/src/js/hamburger-drawer.js
+++ components/drawers/src/js/hamburger-drawer.js
@@ -1,21 +1,24 @@
+/**
+ * Handle hamburger drawer responsiveness.
+ */
 const handleHamburgerDrawer = () => {
-	const drawer_menu_icon = document.querySelector(
+	const drawerMenuIcon = document.querySelector(
 		".hamburger-drawer .drawer-menu-icon"
 	);
 	const drawer = document.querySelector(".hamburger-drawer .drawer");
-	const drawer_content_list = document.querySelector(
+	const drawerContentList = document.querySelector(
 		".hamburger-drawer .drawer .drawer-content--list"
 	);
-	drawer_menu_icon.addEventListener("click", () => {
+	drawerMenuIcon.addEventListener("click", () => {
 		if (
-			drawer_content_list.classList.contains("drawer-menu-icon-inactive") &&
+			drawerContentList.classList.contains("drawer-menu-icon-inactive") &&
 			drawer.classList.contains("drawer-inactive")
 		) {
-			drawer_content_list.classList.remove("drawer-menu-icon-inactive");
+			drawerContentList.classList.remove("drawer-menu-icon-inactive");
 			drawer.classList.remove("drawer-inactive");
 			drawer.classList.add("drawer-active");
 		} else {
-			drawer_content_list.classList.add("drawer-menu-icon-inactive");
+			drawerContentList.classList.add("drawer-menu-icon-inactive");
 			drawer.classList.remove("drawer-active");
 			drawer.classList.add("drawer-inactive");
 		}
