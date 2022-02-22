e2c907b225aaf8e53a3e6653dbe74db4326a2b23
Shreyas Soni <sonishreyas10@gmail.com>
2022-02-22 19:05:34 +0530

    ref: links added to home  and installation page

---
+++ components/installation/src/js/index.js
@@ -0,0 +1,10 @@
+// Index JS
+import { handleNavBar } from "../../../../main/js/handle-nav-bar.js";
+
+/**
+ * Run all scripts on the page
+ */
+const runScripts = () => {
+	handleNavBar();
+};
+runScripts();
